b2a246f66b42610990d3a5e83d6252357e46a318
Marcus Crane <marcus@thingsima.de>
2017-03-02 18:37:06 +1300

    Completed test 4

--- src/04_iqtest.js
+++ src/04_iqtest.js
@@ -1,5 +1,31 @@
+// function iqTest(numbers) {
+//     let input = numbers.split(' ')
+//     let even = []
+//     let odd = []
+//
+//     for (let i = 0; i < input.length; i++) {
+//         if (parseInt(input[i]) % 2 === 0) {
+//             even.push(input[i])
+//         } else {
+//             odd.push(input[i])
+//         }
+//     }
+//
+//     if (even.length > odd.length) {
+//         return input.indexOf(odd[0]) + 1
+//     } else {
+//         return input.indexOf(even[0]) + 1
+//     }
+// }
+
 function iqTest(numbers) {
-    return 3
+    numbers = numbers.split(' ').map((num) => { return parseInt(num) })
+
+    let odd = numbers.filter((num) => { return num % 2 === 1 })
+    let even = numbers.filter((num) => { return num % 2 === 0 })
+
+    return odd.length < even.length ? (numbers.indexOf(odd[0]) + 1) : (numbers.indexOf(even[0]) + 1)
+
 }
 
 module.exports = iqTest
